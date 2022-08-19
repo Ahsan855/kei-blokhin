46e85a91b897bdebd50ded90ee1f079d014bdb06
developer-sahadat <mdridoysikder862@gmail.com>
2022-08-20 01:10:29 +0600

    header section resposive

--- src/Components/Home/HeroSection/HeroSection.js
+++ src/Components/Home/HeroSection/HeroSection.js
@@ -2,31 +2,39 @@ import React from "react";
 
 import hero_img from "../../../Assets/Images/hero_img.png";
 
-
-
 const HeroSection = () => {
   return (
-    <section className="bg-[black] h-auto pb-[39px] pt-[134px] w-full overflow-x-hidden ">
+    <section className=" hero bg-[#0e0d0d] h-auto pb-[39px] pt-[38px] w-full overflow-x-hidden ">
       <div className=" mx-auto flex justify-center items-center  hero-bg">
-        <div className="grid grid-cols-2 justify-center gap-x-[200px]">
-          <div className="text-red-200 w-[476px] font-poppins">
-            <h2 className="font-semibold text-[#FFFFFF] text-[48px]">
+        <div className="grid grid-cols-2 justify-between gap-x-[200px]">
+          <div className=" ml-[170px] font-poppins mx-auto pt-10">
+            <h2 className="font-semibold text-[#FFFFFF] text-[40px] w-[] lg:text-[48px]">
               Welcome to the{" "}
             </h2>
-            <h2 className="font-semibold text-[#FFFFFF] text-[48px]">
+            <h2 className="font-semibold text-[#FFFFFF] text-[40px]  lg:text-[48px]">
               NFT Marketplace
             </h2>
-            <p>
+            <p
+              className="text-[20px] pt-[25px] text-[#FFFFFF] font-normal"
+              style={{ lineHeight: "30px" }}
+            >
               Discover Collections, get Equipment, or find assets to build your
               own unique Experiences.
             </p>
-            <div>
-              <button>Explore</button>
-              <button>Create</button>
+            <div className="pt-[60px]">
+              <button
+                style={{ lineHeight: "36px" }}
+                className="bg-[#3C84F7] text-[24px]  text-[#ffffff] font-semibold font-poppins py-[12px] px-[48px] rounded-[300px]"
+              >
+                Explore
+              </button>
+              <button className=" border-[2px] ml-[16px] border-[#F6D62E] text-[24px]  text-[#ffffff] font-semibold font-poppins py-[12px] px-[48px] rounded-[300px]">
+                Create
+              </button>
             </div>
           </div>
           <div>
-            <img src={hero_img} alt="" />
+            <img src={hero_img} className="w-[366px] h-[448px]" alt="" />
           </div>
         </div>
       </div>
